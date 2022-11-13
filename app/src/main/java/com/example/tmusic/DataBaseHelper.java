12aa9e52f897e7cba42b87b5177379146bcbadc7
SendoaBadiola <72967636+SendoaBadiola@users.noreply.github.com>
2022-11-13 20:49:18 +0100

    Update DataBaseHelper.java

--- app/src/main/java/com/example/tmusic/DataBaseHelper.java
+++ app/src/main/java/com/example/tmusic/DataBaseHelper.java
@@ -1,9 +1,10 @@
 package com.example.tmusic;
-
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -24,7 +25,6 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
     }
 
     @Override
@@ -33,12 +33,35 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     }
 
     Cursor listaAutores(){
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = null;
         if (db != null){
             cursor = db.rawQuery("SELECT * FROM author", null);
         }
         return cursor;
+    }
+
+    Cursor listaCanciones(Integer author){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = null;
+        if(db != null) {
+            cursor = db.rawQuery("SELECT m.song_id, a.name, m.name FROM MUSIC m, AUTHOR a WHERE m.author_id = a.author_id AND m.author_id = "+author, null);
+        }
+        return cursor;
+    }
+
+    public boolean doSignUp(String username, String email, String passwd) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put("username", username);
+        cv.put("email", email);
+        cv.put("passwd", passwd);
+        long result = db.insert("user",null,cv);
+        if (result == -1) {
+
+        }
+        return false;
     }
 
     public boolean checkusernamepassword(String username, String passwod){
